e990e8432479e016d32723b996d411e6a2409cf5
Vas Kaloidis <vas.kaloidis@gmail.com>
2018-02-28 23:12:50 -0500

    Removed user call to projects in the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :load_projects
-
-
-  def load_projects
-    @customer_projects = current_user.customer_projects
-    @owner_projects = current_user.owner_projects
-  end
-
 end
