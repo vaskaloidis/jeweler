9eb9167414a98c0d26361c76577c73e00bed9120
Vas Kaloidis <vas.kaloidis@gmail.com>
2018-04-03 10:46:30 -0400

    Updated routes added webhook routes and ajax routes. Updated app.rb

--- config/application.rb
+++ config/application.rb
@@ -3,14 +3,16 @@ require_relative 'boot'
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+
+unless Rails.env.production?
+  Dotenv::Railtie.load
+end
 
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 
 module JewlerCRM
   class Application < Rails::Application
@@ -24,23 +26,23 @@ module JewlerCRM
 
     config.generators do |g|
       g.factory_bot dir: 'spec/factories'
-      g.test_framework  :rspec, :fixture => true
+      g.test_framework :rspec, :fixture => true
 
     end
 
-      config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
-      config.action_mailer.raise_delivery_errors = true
-      config.action_mailer.delivery_method = :smtp
-      config.action_mailer.default_url_options = { :host => "localhost:3000" }
+    config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = {:host => "localhost:3000"}
 
 
-      ActionMailer::Base.smtp_settings = {
-          :address        => "smtp.gmail.com",
-          :port           => 587,
-          :authentication => :plain,
-          :user_name      => ENV['GMAIL_EMAIL'],
-          :password       => ENV['GMAIL_PASSWORD']
-      }
+    ActionMailer::Base.smtp_settings = {
+        :address => "smtp.gmail.com",
+        :port => 587,
+        :authentication => :plain,
+        :user_name => ENV['GMAIL_EMAIL'],
+        :password => ENV['GMAIL_PASSWORD']
+    }
 
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,19 @@ Rails.application.routes.draw do
 
   # Ajax
   get "/request_payment/:invoice_id"  => 'projects#request_payment', as: 'request_payment'
+  get "/cancel_request_payment/:invoice_id"  => 'projects#cancel_request_payment', as: 'cancel_request_payment'
+
+
   get "/fetch_discussion/:id" => 'discussions#fetch_discussion', as: 'fetch_discussion'
   get "/set_project/:id/current_task/:invoice_item_id" => 'projects#set_current_task', as: 'set_current_task'
+  get "/create_task_inline/:invoice_id" => 'invoice_items#create_task_inline', as: 'create_task_inline'
+
+  # get "/save_task_inline/:invoice_id" => 'InvoiceItems#save_task_inline', as: 'create_task_inline'
+  match 'save_task_inline', to: 'invoice_items#save_task_inline', via: [:post], as: 'save_task_inline'
+
+  get '/oath', to: 'webhook#save_oath', as: 'oath_save'
+  post '/hook', to: 'webhook#hook', as: 'webhook_execute'
+
 
   resources :payments
   resources :invoice_items
