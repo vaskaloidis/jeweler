80bc1309eec2078bbdf4b8f4e872dbe98ae3f39d
Vas Kaloidis <vas.kaloidis@gmail.com>
2018-04-03 10:42:31 -0400

    Added GitHub URL Helpers to App Helper after trying it in other helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def self.github_user(project)
+    uri = URI(project.github_url)
+    return uri.path.split('/').second
+  end
+
+  def self.github_repo(project)
+    uri = URI(project.github_url)
+    return uri.path.split('/').third
+  end
+
 end
