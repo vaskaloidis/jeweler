4a5f5585d42edf4703fe01eb6bb0412c48efabe7
Vas Kaloidis <vas.kaloidis@gmail.com>
2018-05-11 12:11:53 -0400

    Precompiled assets locally

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //  require activestorage
 
 // https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js
-//= require bootstrap.min.js
+// require bootstrap.min.js
 //= require TweenMax.min.js
 //= require resizeable.js
 //= require joinable.js
