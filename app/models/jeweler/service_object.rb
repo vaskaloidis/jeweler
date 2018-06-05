487a1a8d962a9fdfb88d1c9a2ba1e2c15b5f9e79
Vas Kaloidis <vas.kaloidis@gmail.com>
2018-06-05 16:52:26 -0400

    Move work done on Service Objects

--- app/models/jeweler/service_object.rb
+++ app/models/jeweler/service_object.rb
@@ -1,15 +1,25 @@
 module Jeweler
   class ServiceObject
-    include Virtus.model
-    attribute :outcome, Jeweler::ServiceOutcome
-    attr_reader :result
+    # include Virtus.model
+    # attribute :outcome, Jeweler::ServiceOutcome
+    attr_reader :result, :errors, :success, :failure, :success_message, :outcome
 
     def self.call(*args)
       new(*args).tap do |service|
+        se = Jeweler::ServiceOutcome.new
+        service.instance_variable_set(
+            "@errors",
+            []
+        )
+        service_call = service.call
         service.instance_variable_set(
             "@result",
-            service.call
+            service_call
         )
+        se.result = service_call
+        se.errors = service.errors
+        se.success = service.success
+        se.success_message = service.success_message
       end
     end
 
